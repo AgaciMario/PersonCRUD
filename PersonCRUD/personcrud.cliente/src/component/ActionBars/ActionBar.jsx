eebf0c85bb9c351b33952e83b17c7a01fd811311
Agaci Mário <agacimnm@gmail.com>
2025-10-17 14:53:53 -0300

    feat: Adicionando barra de ações.

---
+++ PersonCRUD/personcrud.cliente/src/component/ActionBars/ActionBar.jsx
@@ -0,0 +1,16 @@
+﻿import './ActionBars.css'
+
+function ActionBar() {
+    return (
+        <div className="section actions-bar d-flex flex-wrap justify-content-between align-items-center gap-3">
+            <div className="input-group w-auto flex-grow-1">
+                <span className="input-group-text bg-white"><i className="bi bi-search"></i></span>
+                <input type="text" className="form-control" placeholder="Pesquisar pessoa..." />
+                <button className="btn btn-primary"><i className="bi bi-search me-1"></i>Buscar</button>
+            </div>
+            <button className="btn btn-success"><i className="bi bi-person-plus-fill me-1"></i>Cadastrar Pessoa</button>
+        </div>
+    )
+}
+
+export default ActionBar
--- PersonCRUD/personcrud.cliente/src/pages/PersonPage.jsx
+++ PersonCRUD/personcrud.cliente/src/pages/PersonPage.jsx
@@ -1,11 +1,16 @@
 ﻿// Pagina principal que organiza e importa todos os components
 // também é resposavel pelo gerênciamento de estado como modais abertos paginação, linha selecionada etc
+
+// TODO: explorar a ideia de extrair as tags div que represtam seções para esta pagina, ao invés de telas em cada componente
+
 import Header from '../component/Header/Header'
+import ActionBar from '../component/ActionBars/ActionBar'
 
 function PersonPage() {
     return (
         <div className="container py-4">
             <Header />
+            <ActionBar />
         </div>
     )
 }
