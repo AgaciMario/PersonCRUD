adfd543f72d819b42d98ac2f70bf6f093ec3ce68
Agaci Mário <agacimnm@gmail.com>
2025-11-04 14:21:47 -0300

    feat: Adicionando teste unitário para a classe GetPersonByIdQuery

---
+++ PersonCRUD/PersonCRUD.Tests/PersonCRUD.UnitTests/GetPersonByIdQueryUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using PersonCRUD.Application.Querys.GetPersonByIdQuery;
+
+namespace PersonCRUD.UnitTests
+{
+    public class GetPersonByIdQueryUnitTest
+    {
+        [Theory] // Testes que são verdadeiros somente para um grupo especifico de dados.
+        [InlineData(-1)] // Cada um dos data annotation InlineData será um caso de teste da teoria.
+        [InlineData(0)]
+        public void AceptingOnlyNumberGreaterThanZero(int x)
+        {
+            // Acting and Asserting - realizando operação sobre os dados inicalizados e validando-os:
+            var exception = Assert.Throws<ArgumentException>(() => { GetPersonByIdQuery query = new(x); });
+            Assert.Equal("Id must be greater than zero. (Parameter 'Id')", exception.Message);
+        }
+    }
+}
