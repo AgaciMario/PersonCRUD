4132182bd4e24f29b8d0cc8e6a31f3690118d86f
Agaci Mário <agacimnm@gmail.com>
2025-11-18 15:25:49 -0300

    feat: implementando conceito do endpoint de autenticação.

---
+++ PersonCRUD/PersonCRUD.Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonCRUD.Server.Records;
+
+namespace PersonCRUD.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost(Name = "Authenticate")]
+        //[ProducesResponseType(typeof(PersonDTO), StatusCodes.Status201Created)]
+        //[ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+        //[ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+        public ActionResult Authenticate([FromBody] UserCredentials userCredentials, CancellationToken ct = default)
+        {
+
+            return Ok(userCredentials);
+        }
+    }
+}
