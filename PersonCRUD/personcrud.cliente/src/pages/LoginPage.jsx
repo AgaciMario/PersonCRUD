0bca931f554728fcdee3454e5ca0d77befd959b4
Agaci Mário <agacimnm@gmail.com>
2025-11-27 15:09:07 -0300

    feat: Adicionando UserRepository para comunicação com o endpoint User.

--- PersonCRUD/personcrud.cliente/src/pages/LoginPage.jsx
+++ PersonCRUD/personcrud.cliente/src/pages/LoginPage.jsx
@@ -1,8 +1,32 @@
 ﻿import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Header from '../component/Header/Header';
+import { useState } from 'react';
+import { Login } from '../repository/UserRepository';
+
+// Experiment: tentar implementar usando o react hook forms para ver se há vantagens em utiliza-lo;
+// TODO: realizar uma validação mais completa do campo password, com quantidade minima de caractere, e validação de caracteres variados!
 
 function LoginPage() {
+
+    const [validated, setvalidated ]= useState(false);
+
+    const submitFrom = async (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var form = event.currentTarget;
+        if (form.checkValidity() === false) return;
+
+        setvalidated(true);
+        var userEmail = form[0].value;
+        var usePassword = form[1].value;
+        
+        await Login(userEmail, usePassword)
+            .then((response) => console.log(response))
+            .catch((err) => console.log(err));
+    }
+
     return (
         <div className="grid-center">
             <div className="col-4">
@@ -13,10 +37,11 @@ function LoginPage() {
 
                     <hr></hr>
 
-                    <Form>
+                    <Form onSubmit={submitFrom} noValidate validated={validated}>
                         <Form.Group className="mb-3" controlId="Email">
                             <Form.Label>Email:</Form.Label>
-                            <Form.Control type="email" placeholder="Digite seu email" />
+                            <Form.Control required type="email" placeholder="Digite seu email" />
+                            <Form.Control.Feedback type="invalid">Campo obrigatório!</Form.Control.Feedback>
                             <Form.Text className="text-muted">
                                 Nos não iremos compartilhado seu email com ninguém.
                             </Form.Text>
@@ -24,7 +49,8 @@ function LoginPage() {
 
                         <Form.Group className="mb-3" controlId="Password">
                             <Form.Label>Senha:</Form.Label>
-                            <Form.Control type="password" placeholder="Digite sua senha" />
+                            <Form.Control required type="password" placeholder="Digite sua senha" />
+                            <Form.Control.Feedback type="invalid">Campo obrigatório!</Form.Control.Feedback>
                         </Form.Group>
                         <Button variant="primary" type="submit">
                             <i className="bi bi-box-arrow-in-right me-1"></i>
---
+++ PersonCRUD/personcrud.cliente/src/repository/UserRepository.js
@@ -0,0 +1,17 @@
+﻿import config from "../config/index"
+const URL_PERSON = `${config.URL_BACKEND}/User`;
+
+export async function Login(email, password) {
+    const url = new URL(URL_PERSON + "/Login");
+
+    return await fetch(url, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ email, password }) })
+        .then(async (response) => {
+            const data = await response.json()
+            if (!response.ok) throw new Error(data.Error)
+            return data 
+        })
+        .catch(async (err) => {
+            // TODO: adicionar logger
+            throw new Error(err.message)
+        });
+}
